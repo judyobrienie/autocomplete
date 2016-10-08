a8396721fe08c35c00a9f8697ab43ad5a46a30db
Judy OBrien <judyobrienie@gmail.com>
2016-10-08 19:07:47 +0100

    Refining Data Structures

--- src/autocomplete/BruteAutoComplete.java
+++ src/autocomplete/BruteAutoComplete.java
@@ -7,6 +7,53 @@ import java.util.Scanner;
 
 public class BruteAutoComplete implements AutoComplete {
 
+	List<Integer> weight = new ArrayList<>();
+	List<String> term = new ArrayList<>();
+	
+	
+
+	public  BruteAutoComplete(File usersFile) throws Exception {
+		
+		
+		  Scanner inUsers = new Scanner(usersFile);
+		  String delims = "[ ]";//each field in the file is separated(delimited) by a space.
+		  while (inUsers.hasNextLine()) {
+		    // get user and rating from data source
+		    String userDetails = inUsers.nextLine();
+		    // parse user details string
+		    String[] userTokens = userDetails.split(delims);
+		    Term list = new Term(Integer.parseInt(userTokens[0]), userTokens[1]);
+		    weight = new ArrayList<Integer>();
+		    term = new ArrayList<String>();
+		    // output user data to console.
+		    if (userTokens.length == 2) {
+		    	weight.add(Integer.parseInt(userTokens[0]));
+		    	term.add(userTokens[1]);
+		    	
+		      System.out.println("BruteForceId: " + weight + ",First Name:" + term);
+		      System.out.println(list);
+		      
+		    }else
+		    {
+		      inUsers.close();
+		      throw new Exception("Invalid member length: "+userTokens.length);
+		    }
+		  }
+		  inUsers.close();
+		
+		
+	}
+
+	 public Term createTerm(int weight, String term) 
+	  {
+	    Term terms = new Term (weight, term);
+	    
+	    return terms;
+	    
+	  }
+		
+	
+	
 	
 	
 	
--- src/autocomplete/Main.java
+++ src/autocomplete/Main.java
@@ -1,5 +1,6 @@
 package autocomplete;
 
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -9,9 +10,11 @@ import java.util.Scanner;
 
 
 
+
+
 public class Main {
 
-	
+	public static final File usersFile = new File("../autocomplete/lib/test.txt");
 		
 		
 
@@ -19,42 +22,23 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		
-		AutoCompleteAPI AutoCompleteAPI = new AutoCompleteAPI();
+		QuickAutoComplete AutoComplete = new QuickAutoComplete(usersFile);
+		
 
-		 File usersFile = new File("../autocomplete/lib/test.txt");
-		  Scanner inUsers = new Scanner(usersFile);
-		  String delims = "[ ]";//each field in the file is separated(delimited) by a space.
-		  while (inUsers.hasNextLine()) {
-		    
-			  // get user and rating from data source
-		    String data = inUsers.nextLine();
-		    // parse user details string
-		      String[] arrayTerms = data.split(delims);
-		     
-		 
-		    
-		    // output user data to console.
-		    if (arrayTerms.length == 2) {
-		     
-		      System.out.println("Weight: " + arrayTerms[0] + "    Name:  " + arrayTerms[1]);
-		      AutoCompleteAPI.createTerm(Integer.parseInt(arrayTerms[0]), arrayTerms[1]);
-		    }else
-		    {
-		      inUsers.close();
-		      throw new Exception("Invalid member length: "+ arrayTerms.length);
-		    }
-		  }
-		  inUsers.close();
+		 AutoComplete BruteAutoComplete = new BruteAutoComplete(usersFile);
+		  
 		
 		
 		
 		
-		
-	    Collection<Term> terms = AutoCompleteAPI.getTerms();
-	    System.out.println(terms);
+		    Collection<Term> terms = QuickAutoComplete.getTerms();
+		    System.out.println(terms);
 	    
-	
+	   
+	    
 	  
 	}
+
+	
 	   
 	  }
