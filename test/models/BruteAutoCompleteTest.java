2207af55b92ac0646d2a21baa0385f078615f33c
Judy OBrien <judyobrienie@gmail.com>
2016-10-08 17:00:17 +0100

    Test

---
+++ test/models/BruteAutoCompleteTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BruteAutoCompleteTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
